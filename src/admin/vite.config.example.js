1a641355d5343fa6241bd7dd61c6d486bf00222b
CoderKashyap <kashyap.abhi1105@gmail.com>
2025-06-11 18:13:17 +0530

    allow all host from vite

--- src/admin/vite.config.example.js
+++ src/admin/vite.config.example.js
@@ -3,6 +3,11 @@ const { mergeConfig } = require('vite');
 module.exports = (config) => {
   // Important: always return the modified config
   return mergeConfig(config, {
+     server: {
+      allowedHosts: 'all', // Allows all hosts
+      host: '0.0.0.0',
+      port: 1337
+    },
     resolve: {
       alias: {
         '@': '/src',
