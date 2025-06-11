c338c23feea1ce99567584174a7dcd15de4a363d
CoderKashyap <kashyap.abhi1105@gmail.com>
2025-06-11 11:28:14 +0530

    proceed with sqlite

--- config/database.js
+++ config/database.js
@@ -1,29 +1,31 @@
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
-    connection: {
-      host: env('DATABASE_HOST', 'db.dixjjzfbnmcizaxjhtwm.supabase.co'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'postgres'),
-      user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD'),
-      ssl: {
-        rejectUnauthorized: false, // Required for Supabase
-      },
-    },
-    acquireConnectionTimeout: 60000,
-    pool: {
-      min: 0,
-      max: 10,
-      createTimeoutMillis: 30000,
-      acquireTimeoutMillis: 60000,
-      idleTimeoutMillis: 30000,
-      reapIntervalMillis: 1000,
-      createRetryIntervalMillis: 100,
-    },
-    debug: false,
-  },
-});
+// Method 1
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'postgres',
+//     connection: {
+//       host: env('DATABASE_HOST', 'db.dixjjzfbnmcizaxjhtwm.supabase.co'),
+//       port: env.int('DATABASE_PORT', 5432),
+//       database: env('DATABASE_NAME', 'postgres'),
+//       user: env('DATABASE_USERNAME', 'postgres'),
+//       password: env('DATABASE_PASSWORD'),
+//       ssl: {
+//         rejectUnauthorized: false, // Required for Supabase
+//       },
+//     },
+//     acquireConnectionTimeout: 60000,
+//     pool: {
+//       min: 0,
+//       max: 10,
+//       createTimeoutMillis: 30000,
+//       acquireTimeoutMillis: 60000,
+//       idleTimeoutMillis: 30000,
+//       reapIntervalMillis: 1000,
+//       createRetryIntervalMillis: 100,
+//     },
+//     debug: false,
+//   },
+// });
 
 
 
@@ -36,7 +38,7 @@ module.exports = ({ env }) => ({
 
 
 
-
+// Method 2
 // const parse = require("pg-connection-string").parse;
 
 // module.exports = ({ env }) => {
@@ -61,87 +63,87 @@ module.exports = ({ env }) => ({
 // };
 
 
-// const path = require('path');
-
-// module.exports = ({ env }) => {
-//   const client = env('DATABASE_CLIENT', 'postgres');
-//   // const client = env('DATABASE_CLIENT', 'sqlite');
-
-//   const connections = {
-//   //   mysql: {
-//   //     connection: {
-//   //       host: env('DATABASE_HOST', 'localhost'),
-//   //       port: env.int('DATABASE_PORT', 3306),
-//   //       database: env('DATABASE_NAME', 'strapi'),
-//   //       user: env('DATABASE_USERNAME', 'strapi'),
-//   //       password: env('DATABASE_PASSWORD', 'strapi'),
-//   //       ssl: env.bool('DATABASE_SSL', false) && {
-//   //         key: env('DATABASE_SSL_KEY', undefined),
-//   //         cert: env('DATABASE_SSL_CERT', undefined),
-//   //         ca: env('DATABASE_SSL_CA', undefined),
-//   //         capath: env('DATABASE_SSL_CAPATH', undefined),
-//   //         cipher: env('DATABASE_SSL_CIPHER', undefined),
-//   //         rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
-//   //       },
-//   //     },
-//   //     pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
-//   //   },
 
 
+const path = require('path');
 
+module.exports = ({ env }) => {
+  const client = env('DATABASE_CLIENT', 'sqlite');
 
-//     //     module.exports = ({ env }) => ({
-//     //   connection: {
-//     //     client: 'postgres',
-//     //     connection: {
-//     //       host: env('DATABASE_HOST', 'your-project-ref.supabase.co'),
-//     //       port: env.int('DATABASE_PORT', 5432),
-//     //       database: env('DATABASE_NAME', 'postgres'),
-//     //       user: env('DATABASE_USERNAME', 'postgres'),
-//     //       password: env('DATABASE_PASSWORD', 'your-password'),
-//     //       ssl: {
-//     //         rejectUnauthorized: false,  // Important for Supabase
-//     //       },
-//     //     },
-//     //     debug: false,
-//     //   },
-//     // });
+  const connections = {
+  //   mysql: {
+  //     connection: {
+  //       host: env('DATABASE_HOST', 'localhost'),
+  //       port: env.int('DATABASE_PORT', 3306),
+  //       database: env('DATABASE_NAME', 'strapi'),
+  //       user: env('DATABASE_USERNAME', 'strapi'),
+  //       password: env('DATABASE_PASSWORD', 'strapi'),
+  //       ssl: env.bool('DATABASE_SSL', false) && {
+  //         key: env('DATABASE_SSL_KEY', undefined),
+  //         cert: env('DATABASE_SSL_CERT', undefined),
+  //         ca: env('DATABASE_SSL_CA', undefined),
+  //         capath: env('DATABASE_SSL_CAPATH', undefined),
+  //         cipher: env('DATABASE_SSL_CIPHER', undefined),
+  //         rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
+  //       },
+  //     },
+  //     pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
+  //   },
 
 
 
-//     postgres: {
-//       connection: {
-//         connectionString: env('DATABASE_URL'),
-//         host: env('DATABASE_HOST', 'localhost'),
-//         port: env.int('DATABASE_PORT', 5432),
-//         database: env('DATABASE_NAME', 'strapi'),
-//         user: env('DATABASE_USERNAME', 'strapi'),
-//         password: env('DATABASE_PASSWORD', 'strapi'),
-//         ssl: env.bool('DATABASE_SSL', false) && {
-//           key: env('DATABASE_SSL_KEY', undefined),
-//           cert: env('DATABASE_SSL_CERT', undefined),
-//           ca: env('DATABASE_SSL_CA', undefined),
-//           capath: env('DATABASE_SSL_CAPATH', undefined),
-//           cipher: env('DATABASE_SSL_CIPHER', undefined),
-//           rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
-//         },
-//         schema: env('DATABASE_SCHEMA', 'public'),
-//       },
-//       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
-//     },
-//     sqlite: {
-//       connection: {
-//         filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-//       },
-//       useNullAsDefault: true,
-//     },
-//   };
 
-//   return {
-//     connection: {
-//       client,
-//       ...connections[client],
-//       acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
-//     },
-//   };
-// };
+    //     module.exports = ({ env }) => ({
+    //   connection: {
+    //     client: 'postgres',
+    //     connection: {
+    //       host: env('DATABASE_HOST', 'your-project-ref.supabase.co'),
+    //       port: env.int('DATABASE_PORT', 5432),
+    //       database: env('DATABASE_NAME', 'postgres'),
+    //       user: env('DATABASE_USERNAME', 'postgres'),
+    //       password: env('DATABASE_PASSWORD', 'your-password'),
+    //       ssl: {
+    //         rejectUnauthorized: false,  // Important for Supabase
+    //       },
+    //     },
+    //     debug: false,
+    //   },
+    // });
+
+    // postgres: {
+    //   connection: {
+    //     connectionString: env('DATABASE_URL'),
+    //     host: env('DATABASE_HOST', 'localhost'),
+    //     port: env.int('DATABASE_PORT', 5432),
+    //     database: env('DATABASE_NAME', 'strapi'),
+    //     user: env('DATABASE_USERNAME', 'strapi'),
+    //     password: env('DATABASE_PASSWORD', 'strapi'),
+    //     ssl: env.bool('DATABASE_SSL', false) && {
+    //       key: env('DATABASE_SSL_KEY', undefined),
+    //       cert: env('DATABASE_SSL_CERT', undefined),
+    //       ca: env('DATABASE_SSL_CA', undefined),
+    //       capath: env('DATABASE_SSL_CAPATH', undefined),
+    //       cipher: env('DATABASE_SSL_CIPHER', undefined),
+    //       rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
+    //     },
+    //     schema: env('DATABASE_SCHEMA', 'public'),
+    //   },
+    //   pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
+    // },
+
+    sqlite: {
+      connection: {
+        filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      },
+      useNullAsDefault: true,
+    },
+  };
+
+  return {
+    connection: {
+      client,
+      ...connections[client],
+      acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
+    },
+  };
+};
